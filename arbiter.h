cfb6798cbfc108a67c540b9f9485140d3778789f
Sophie <sophie.barlet@gmail.com>
2014-11-30 15:41:29 -0500

    adjust arbiter w.r.t others

--- arbiter.h
+++ arbiter.h
@@ -4,9 +4,12 @@
 #include <map>
 #include <math.h>
 #include <vector>  
+#include <utility>
 #include "DataStructures.h"
 #include "wrapper.hpp"
 #include "Spline.h"
+#include "policy.h"
+#include "controller.h"
 using namespace std;
 
 class Arbiter
@@ -14,22 +17,23 @@ class Arbiter
 public:
 	Arbiter(int nb_segments, int nb_intervals_per_segment):m_nb_segments(nb_segments),m_nb_intervals_per_segment(nb_intervals_per_segment){};
 	Arbiter(int nb_segments, int nb_intervals_per_segment, 
-		Controller* controller, Policy* policy, Wrapper* wrapper);
+		Driver* controller, ZeroPolicy* policy, Wrapper* wrapper);
 	void drive();
-	void setController(Controller* controller);
+	void setController(Driver* controller);
 	void setWrapper(Wrapper* w);
-	void setPolicy(Policy* policy);
+	void setPolicy(ZeroPolicy* policy);
 private:
-	Controller* m_controller;
-	Policy* m_policy;
+	Driver* m_controller;
+	ZeroPolicy* m_policy;
 	Wrapper* m_wrapper;
 	float m_track_length;
 	int m_nb_segments;
 	float m_segment_length;
 	float m_interval_length;
 	int m_nb_intervals_per_segment;
-	map<float, WorldState> m_positions;
-	map<float,vector<float> > m_positions_x;
-	void collectIntervalPositions(float, float);
+	map<int, vector<double> > m_curvatures;
+	map<int,vector<double> > m_positions_x;
+	void collectIntervalPositions(int, double, double);
+	pair<vector<double>, vector<double> > getSubCurvatures(int, float);
 };
 #endif
